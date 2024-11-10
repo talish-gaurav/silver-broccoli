e773f8b14c75c27bb0dff1fcab9a64fbeefd8387
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-11-10 07:54:33 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+num = int(input("Please write your numerator here"))
+
+den = int(input("Please write your denominator here"))
+
+if num%den == 0:
+
+      print("{0} is divisible by{1} ".format(num,den))
+
+else:
+      
+      print("{0} is not divisible by{1} ". format(num,den))
